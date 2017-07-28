ff33246ec75aa8dc45f388a466b4273b5e6f173e
Steve Mckellar <steve@wwqrd.uk>
2017-07-28 12:47:20 +0100

    Server tests WIP

--- public/components/Server.js
+++ public/components/Server.js
@@ -10,11 +10,16 @@ const events = ['data'];
 class Server extends Emitter {
   constructor(port, options = { keys: null }) {
     super();
+    if (!port) return;
     this.options = options;
     this.started = new Date().getTime();
     this.server = io(port);
     this.listen();
     console.log(`Server started on port ${port}.`);
+  }
+
+  close() {
+    this.server.close();
   }
 
   listen() {
@@ -25,25 +30,17 @@ class Server extends Emitter {
       console.log('Private socket established, listening...');
 
       ps.on('data', (data) => {
-        // TODO: Could store data here?
+        // TODO: Could store data here or in some kind of HOC?
         this.emit('data', data);
       });
     });
   }
 
-  clients() {
-    return Object.keys(this.server.sockets.sockets).length;
-  }
-
-  ip() {
-    return os.networkInterfaces();
-  }
-
   status() {
     return {
-      started: this.started,
-      ip: this.ip(),
-      clients: this.clients(),
+      uptime: new Date().getTime() - this.started,
+      ip: os.networkInterfaces(),
+      clients: Object.keys(this.server.sockets.sockets).length,
       publicKey: this.options.keys.publicKey,
     };
   }
