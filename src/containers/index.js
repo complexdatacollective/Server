b44a4da47ace038bf5e978e19f470cb9d008aad2
Jamie Chung <jchung@atlassian.com>
2017-09-21 14:29:16 -0500

    Removing ipc component.

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,4 +2,3 @@ export { default as App } from './App';
 export { default as ExportScreen } from './ExportScreen';
 export { default as OverviewDashboard } from './OverviewDashboard';
 export { default as OverviewScreen } from './OverviewScreen';
-export { default as icp } from './ipc';
