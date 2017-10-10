67ec4fc497df03875149cd0bc58d0a8730ed9521
Jamie Chung <jchung@atlassian.com>
2017-10-10 16:02:40 -0500

    Removing console for lint errors.

--- src/utils/Server.js
+++ src/utils/Server.js
@@ -6,7 +6,6 @@ class Server {
   constructor() {
     this.events = new EventEmitter();
     socket.addEventListener('message', (event) => {
-      console.log('Message from server ', event.data);
       this.events.emit('SERVER_STATUS', event.data);
     });
   }
@@ -16,10 +15,8 @@ class Server {
   }
 
   requestServerStatus = () => {
-    console.log('REQUESTING');
     // Connection opened
     socket.addEventListener('open', (event) => {
-      console.log(event);
       socket.send('REQUEST_SERVER_STATUS');
     });
   }
