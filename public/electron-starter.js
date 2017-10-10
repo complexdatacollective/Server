79422a4bf0763d4636e278e1cb79d6ffd29001f6
Jamie Chung <jchung@atlassian.com>
2017-10-10 15:53:44 -0500

    Removing extraneous commands for electron starter.

--- public/electron-starter.js
+++ public/electron-starter.js
@@ -1,32 +1,11 @@
 const { app } = require('electron');
-// const path = require('path');
 const { createMainWindow } = require('./components/mainWindow');
 const { createTray } = require('./components/tray');
-// const { createServer } = require('./components/serverManager');
 
-// const settingsDb = path.join(app.getPath('userData'), 'db', 'settings');
-
-// const server = null;
+// start the server
+require('./server-starter');
 
 const mainWindow = createMainWindow();
-
-// createServer(8080, settingsDb)
-// .then((serverProcess) => {
-//   server = serverProcess;
-//   console.log(server);
-//   ipcMain.on('REQUEST_SERVER_STATUS', (event) => {
-//     console.log('request received', event);
-//     serverProcess.on(
-//       'SERVER_STATUS',
-//       ({ data }) => {
-//         console.log(data);
-//         event.sender.send('SERVER_STATUS', data);
-//       }
-//     );
-
-//     serverProcess.send({ action: 'REQUEST_SERVER_STATUS' });
-//   });
-// });
 
 const trayMenu = [
   {
@@ -43,12 +22,6 @@ const trayMenu = [
   },
 ];
 
-// ipcMain.on('REQUEST_SERVER_OVERVIEW', () => {
-//   if (!server) { return; }
-
-//   server.send({ action: 'REQUEST_SERVER_STATUS' });
-// });
-
 app.on('ready', () => {
   app.dock.hide();
   createTray(trayMenu);
@@ -56,10 +29,3 @@ app.on('ready', () => {
 
 // Don't quit when all windows are closed.
 app.on('window-all-closed', () => { });  // no op
-
-app.on('before-quit', () => {
-  if (server) {
-    // Quit spawned server process
-    server.stop();
-  }
-});
