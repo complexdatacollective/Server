0fc161a3fe72711efe801705da9cba7c1eefdeeb
Jamie Chung <jchung@atlassian.com>
2017-12-06 14:03:09 -0800

    Adding unpaired device publisher.

--- src/utils/Server.js
+++ src/utils/Server.js
@@ -3,16 +3,17 @@ import io from 'socket.io-client';
 
 class Server {
   constructor() {
-    this.socket = io('http://localhost:8080', {
-    });
+    this.socket = io('http://localhost:8080');
+    this.discoverySocket = io('http://localhost:8080/disc');
 
     this.events = new EventEmitter();
     this.socket.on('SERVER_STATUS', (data) => {
       console.log('SERVER_STATUS', data);
       this.events.emit('SERVER_STATUS', data);
     });
-    this.socket.on('discoveryUpdate', (data) => {
-      console.log('update', data);
+
+    this.discoverySocket.on('unpairedDevice', (data) => {
+      console.log('unpaired device', data);
     });
   }
 
@@ -23,13 +24,14 @@ class Server {
   requestServerStatus = () => {
     const req = {
       deviceName: 'device-name',
-
+      protocol: 'protocol-name',
+      reqDate: new Date(),
     };
 
     // // Connection opened
     this.socket.on('connect', () => {
       this.socket.emit('REQUEST_SERVER_STATUS');
-      this.socket.emit('discoveryRequest', req, (data) => {
+      this.discoverySocket.emit('discoveryRequest', req, (data) => {
         console.log(req);
         console.log('normal', data);
       });
