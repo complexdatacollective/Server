7b08e53ccaf587a178cae0fbab93ce80450c0342
Bryan Fox <git@bryfox.com>
2018-03-21 12:55:39 -0400

    Update dependencies causing install warnings
    Remove unused dependencies (from Network-Canvas)

--- src/renderer/ducks/modules/rootReducer.js
+++ src/renderer/ducks/modules/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
 
 import pairingReducer from './pairingRequest';
 
 const appReducer = combineReducers({
-  form: formReducer,
   pairingRequest: pairingReducer,
 });
 
