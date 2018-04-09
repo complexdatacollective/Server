a8ddfad11050a9c042e2ac199e4da3c879ef1295
Bryan Fox <git@bryfox.com>
2018-04-09 13:09:37 -0400

    Add coverage for Protocol contents

--- src/main/data-managers/__tests__/ProtocolManager-test.js
+++ src/main/data-managers/__tests__/ProtocolManager-test.js
@@ -10,9 +10,11 @@ jest.mock('electron-log');
 describe('ProtocolManager', () => {
   const errorMessages = ProtocolManager.ErrorMessages;
   let importer;
+  let invalidFileErr;
 
   beforeEach(() => {
     importer = new ProtocolManager('.');
+    invalidFileErr = expect.objectContaining({ message: errorMessages.InvalidFile });
   });
 
   describe('UI hook', () => {
@@ -24,13 +26,13 @@ describe('ProtocolManager', () => {
 
   describe('import interface', () => {
     it('requires files to import', async () => {
-      const invalidFileErr = expect.objectContaining({ message: errorMessages.EmptyFilelist });
-      await expect(importer.validateAndImport()).rejects.toEqual(invalidFileErr);
+      const emptyErr = expect.objectContaining({ message: errorMessages.EmptyFilelist });
+      await expect(importer.validateAndImport()).rejects.toEqual(emptyErr);
     });
 
     it('makes a directory if needed', () => {
       importer.validateAndImport(['foo.netcanvas']);
-      expect(fs.mkdir).toHaveBeenCalled();
+      expect(fs.mkdir).toHaveBeenCalledWith(importer.protocolDir, expect.any(Function));
     });
 
     describe('with a valid directory', () => {
@@ -39,7 +41,6 @@ describe('ProtocolManager', () => {
       });
 
       it('requires a valid file extension', async () => {
-        const invalidFileErr = expect.objectContaining({ message: errorMessages.InvalidFile });
         await expect(importer.validateAndImport(['file.unknownextension'])).rejects.toEqual(invalidFileErr);
       });
 
@@ -56,6 +57,41 @@ describe('ProtocolManager', () => {
     });
   });
 
+  describe('ensureDataDir()', () => {
+    describe('when mkdir errors', () => {
+      let existErr;
+      beforeAll(() => {
+        fs.mkdir.mockImplementation((dir, cb) => {
+          cb(existErr);
+        });
+      });
+
+      beforeEach(() => {
+        existErr = new Error();
+      });
+
+      describe('with EEXIST', () => {
+        beforeEach(() => {
+          existErr.code = 'EEXIST';
+        });
+
+        it('resolves with the directory', async () => {
+          await expect(importer.ensureDataDir()).resolves.toMatch(importer.protocolDir);
+        });
+      });
+
+      describe('with an unexpected error', () => {
+        beforeEach(() => {
+          existErr.message = 'Unexpected Error';
+        });
+
+        it('rejects', async () => {
+          await expect(importer.ensureDataDir()).rejects.toMatchObject(existErr);
+        });
+      });
+    });
+  });
+
   it('uses fs to copy a file', () => {
     importer.importFile('foo.netcanvas');
     expect(fs.copyFile)
@@ -63,15 +99,12 @@ describe('ProtocolManager', () => {
   });
 
   it('requires a filename', async () => {
-    const invalidFileErr = expect.objectContaining({ message: errorMessages.InvalidFile });
     await expect(importer.importFile()).rejects.toEqual(invalidFileErr);
   });
 
-  it('resolves with the destination filename', (done) => {
+  it('resolves with the destination filename', async () => {
     fs.copyFile = jest.fn((src, dest, cb) => { cb(); });
-    importer.importFile('foo.netcanvas')
-      .then(file => expect(file).toMatch(/foo/))
-      .then(done);
+    await expect(importer.importFile('foo.netcanvas')).resolves.toMatch(/foo/);
   });
 
   it('rejects on failure', async () => {
@@ -80,11 +113,44 @@ describe('ProtocolManager', () => {
     await expect(importer.importFile('foo.netcanvas')).rejects.toEqual(err);
   });
 
-  it('uses fs to find existing files', (done) => {
+  it('uses fs to find existing files', async () => {
     const mockFiles = ['a.netcanvas'];
     fs.readdir = jest.fn((dir, cb) => { cb(null, mockFiles); });
-    importer.savedFiles()
-      .then(files => expect(files).toEqual(mockFiles))
-      .then(done);
+    await expect(importer.savedFiles()).resolves.toEqual(mockFiles);
+  });
+
+  describe('fileContents', () => {
+    it('rejects on missing input', async () => {
+      await expect(importer.fileContents()).rejects.toMatchObject(invalidFileErr);
+    });
+
+    it('will not try to read file outside directory', async () => {
+      expect(fs.readFile).not.toHaveBeenCalled();
+      await expect(importer.fileContents('../insecureaccess')).rejects.toMatchObject(invalidFileErr);
+    });
+
+    it('returns raw content buffer', async () => {
+      expect.assertions(2);
+      const mockFileContents = new Buffer([]);
+      fs.readFile.mockImplementation((file, opts, cb) => {
+        const fn = typeof cb === 'undefined' ? opts : cb;
+        expect(opts.encoding).toBeUndefined();
+        fn(null, mockFileContents);
+      });
+      await expect(importer.fileContents('a.netcanvas')).resolves.toEqual(mockFileContents);
+    });
+
+    describe('when file does not exist', () => {
+      beforeAll(() => {
+        fs.readFile.mockImplementation((file, opts, cb) => {
+          const fn = typeof cb === 'undefined' ? opts : cb;
+          fn(invalidFileErr);
+        });
+      });
+
+      it('rejects when file is missing', async () => {
+        await expect(importer.fileContents('missingfile')).rejects.toMatchObject(invalidFileErr);
+      });
+    });
   });
 });
