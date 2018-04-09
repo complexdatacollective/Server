e495bbb31179d97b86db73d76bd3a10a539c32cb
Bryan Fox <git@bryfox.com>
2018-04-09 13:35:58 -0400

    Update tests for api client

--- src/renderer/utils/__tests__/adminApiClient-test.js
+++ src/renderer/utils/__tests__/adminApiClient-test.js
@@ -8,17 +8,31 @@ describe('an AdminApiClient', () => {
     client = new AdminApiClient();
   });
 
+  afterEach(() => {
+    fetch.resetMocks();
+  });
+
   it('can get the server status', (done) => {
     const mockStatus = { uptime: 100 };
-
     fetch.mockResponse(JSON.stringify({ serverStatus: mockStatus }));
-
     client.get('/health')
       .then((data) => {
         expect(data).toHaveProperty('serverStatus');
         expect(data.serverStatus).toMatchObject(mockStatus);
       })
       .then(done);
+  });
+
+  it('can post data', async () => {
+    const payload = { foo: 'bar' };
+    fetch.mockResponse(JSON.stringify({ status: 'ok', data: payload }));
+    await expect(client.post('/foo', payload)).resolves.toMatchObject({ status: 'ok' });
+  });
+
+  it('rejects on fetch error', async () => {
+    const notFoundErr = new Error('Not Found');
+    fetch.mockReject(notFoundErr);
+    await expect(client.post('/foo')).rejects.toMatchObject(notFoundErr);
   });
 
   it('rejects on server error', async () => {
