d293c121aa2cc4a35ea48f0b6bbd2c7d5d312312
Bryan Fox <git@bryfox.com>
2018-04-13 14:19:44 -0400

    Mock createServer for index test
    Fixes hang in CI

--- src/main/__tests__/index-test.js
+++ src/main/__tests__/index-test.js
@@ -3,6 +3,12 @@
 require('../index');
 const { Menu } = require('electron');
 
+// Don't start the server for these tests
+jest.mock('../worker/serverManager', () => ({
+  actions: {},
+  createServer: jest.fn().mockResolvedValue({}),
+}));
+
 jest.mock('electron');
 
 describe('the electron app', () => {
