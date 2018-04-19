5ac09d5874314e502dab97c1287f809167e8f1f6
Bryan Fox <git@bryfox.com>
2018-04-19 18:11:54 -0400

    Add basic usability through native menu
    This also (automatically) adds standard keyboard shortcuts.

--- src/main/MainApp.js
+++ src/main/MainApp.js
@@ -58,6 +58,37 @@ const createApp = () => {
         },
       ],
     },
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { type: 'separator' },
+        { role: 'selectall' },
+      ],
+    },
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forcereload' },
+        { role: 'toggledevtools' },
+        { type: 'separator' },
+        { role: 'resetzoom' },
+        { role: 'zoomin' },
+        { role: 'zoomout' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' },
+      ],
+    },
+    {
+      role: 'window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'close' },
+      ],
+    },
   ]);
 
   app.on('ready', () => {
