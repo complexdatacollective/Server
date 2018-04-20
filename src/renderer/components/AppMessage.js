8601b3f64ffbc149efbb1b14136aa8f10b73c6d5
Bryan Fox <git@bryfox.com>
2018-04-19 21:51:04 -0400

    Add basic transition for flash

--- src/renderer/components/AppMessage.js
+++ src/renderer/components/AppMessage.js
@@ -2,13 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Icon } from 'network-canvas-ui';
 
+import Fade from './transitions/Fade';
+
 const AppMessage = ({ text }) => (
-  <div className="app-message">
-    <Icon name="close" size="small" color="red" className="app-message__close" />
-    <div className="app-message__text">
-      {text}
+  <Fade transitionIn>
+    <div className="app-message">
+      <Icon name="close" size="small" color="red" className="app-message__close" />
+      <div className="app-message__text">
+        {text}
+      </div>
     </div>
-  </div>
+  </Fade>
 );
 
 AppMessage.propTypes = {
