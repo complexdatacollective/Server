34c324602d20f7ffc0f5d563a4c36fb3c9ec1404
Joshua Melville <joshua.melville@northwestern.edu>
2018-05-08 13:59:24 +0100

    tidy up icon generation script

--- scripts/generate-app-icons.js
+++ scripts/generate-app-icons.js
@@ -5,20 +5,20 @@ const path = require('path');
 const svg2png = require('svg2png');
 
 const jobList = [
-  // {
-  //   name: 'Electron Main App Icon',
-  //   runner: 'icongen',
-  //   inputFile: 'assets/icons/Srv-Desktop.svg',
-  //   outputPath: 'build-resources',
-  //   options: {
-  //     modes: ['ico', 'icns'], // all
-  //     names: {
-  //       ico: 'icon',
-  //       icns: 'icon',
-  //     },
-  //     report: true,
-  //   },
-  // },
+  {
+    name: 'Electron Main App Icon',
+    runner: 'icongen',
+    inputFile: 'assets/icons/Srv-Desktop.svg',
+    outputPath: 'build-resources',
+    options: {
+      modes: ['ico', 'icns'], // all
+      names: {
+        ico: 'icon',
+        icns: 'icon',
+      },
+      report: true,
+    },
+  },
   {
     name: 'Electron Tray Icon (macOS)',
     runner: 'svg2png',
@@ -49,46 +49,37 @@ const jobList = [
 ];
 
 const icongenTask = (specification) => {
-  console.log('icon-gen task');
-  console.log(specification);
   icongen(specification.inputFile, specification.outputPath, specification.options)
-    .then((results) => {
-      console.log(results);
-    })
     .catch((err) => {
-      console.log(err);
+      throw err;
     });
 };
 
 const svg2pngTask = (specification) => {
-  console.log('SVG2PNG task');
-  console.log(specification);
   const buffer = fs.readFileSync(specification.inputFile);
 
   if (!buffer) {
     throw Error(`Failed to write the image ${specification.size} x ${specification.size}`);
   }
 
-  Object.entries(specification.sizes).forEach((size) => {
-    console.log(`size ${size[1]}`);
-    let dest = `${specification.fileName}${specification.append[specification.sizes.indexOf(size[1])]}.png`;
+  specification.sizes.forEach((size) => {
+    let dest = `${specification.fileName}${specification.append[specification.sizes.indexOf(size)]}.png`;
     dest = path.join(specification.outputPath + dest);
-    console.log(`dest ${dest}`);
 
-    svg2png(buffer, { width: size[1], height: size[1] }).then((output) => {
+    svg2png(buffer, { width: size, height: size }).then((output) => {
       fs.writeFile(dest, output, (err) => {
-        if (err) console.log(err);
+        if (err) throw err;
       });
     });
   });
 };
 
 const parseJobs = (jobs) => {
-  Object.entries(jobs).forEach((job) => {
-    if (job[1].runner === 'icongen') {
-      icongenTask(job[1]);
+  jobs.forEach((job) => {
+    if (job.runner === 'icongen') {
+      icongenTask(job);
     } else {
-      svg2pngTask(job[1]);
+      svg2pngTask(job);
     }
   });
 };
