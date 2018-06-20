a5e2d80307eec8af32abe9051ee8cd1756604b1b
Bryan Fox <git@bryfox.com>
2018-06-20 13:53:52 -0400

    Support browser dev in environment check

--- src/renderer/utils/environment.js
+++ src/renderer/utils/environment.js
@@ -1,6 +1,6 @@
 import { remote } from 'electron';
 
-const isFrameless = () => remote.process.platform === 'darwin';
+const isFrameless = () => remote && remote.process.platform === 'darwin';
 
 export {
   isFrameless, // eslint-disable-line import/prefer-default-export
