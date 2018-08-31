169efccef87d705dd3294542544cb66c43fba81e
Bryan Fox <git@bryfox.com>
2018-08-31 12:25:23 -0400

    Make it harder to accidentally close Pairing Code
    If a user double-clicks or otherwise unintentionally clicks the modal
    background, then the pairing code is lost and cannot be retrieved on the
    UI and they would need to restart the pairing process.
    
    Partial fix for #163.

---
+++ src/renderer/components/__tests__/Modal-test.js
@@ -0,0 +1,24 @@
+/* eslint-env jest */
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Modal from '../Modal';
+
+const props = {
+  title: 'mock',
+  onCancel: jest.fn(),
+};
+
+describe('Modal', () => {
+  it('is not closed when clicking the background', () => {
+    const modal = shallow(<Modal {...props} />);
+    modal.find('.modal').simulate('click');
+    expect(props.onCancel).not.toHaveBeenCalled();
+  });
+
+  it('is closed when clicking the cancel button', () => {
+    const modal = shallow(<Modal {...props} />);
+    modal.find('Button').simulate('click');
+    expect(props.onCancel).toHaveBeenCalled();
+  });
+});
