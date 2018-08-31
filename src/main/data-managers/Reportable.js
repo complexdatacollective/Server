02439dcd7b0b48b3520607ae3e22358a96fcdad4
Bryan Fox <git@bryfox.com>
2018-08-31 10:09:26 -0400

    Fix count reducer; add basic data test cases

--- src/main/data-managers/Reportable.js
+++ src/main/data-managers/Reportable.js
@@ -1,4 +1,10 @@
-const { resolveWithDocCount, resolveOrReject } = require('../utils/db');
+/* eslint-disable no-param-reassign */ // disabled for reducers
+const { resolveOrReject } = require('../utils/db');
+
+// This is a dummy field used for projections when counting network members.
+// When we don't need to know anything about members other than size, returning
+// an empty dummy field instead of full objects speeds up counting significantly.
+const pseudoField = 'nc_pseudo_field';
 
 /**
  * Mixin for report queries on a SessionDB.
@@ -28,8 +34,11 @@ const Reportable = Super => class extends Super {
     return new Promise((resolve, reject) => {
       this.db
         .find({ protocolId, 'data.nodes': { $exists: true } })
-        .projection({ 'data.nodes': 1, _id: 0 })
-        .exec(resolveOrReject(resolveWithDocCount(resolve), reject));
+        .projection({ [`data.nodes.${[pseudoField]}`]: 1, _id: 0 })
+        .exec(resolveOrReject(docs => resolve(docs.reduce((sum, doc) => {
+          sum += doc.data.nodes[pseudoField].length;
+          return sum;
+        }, 0)), reject));
     });
   }
 
@@ -37,8 +46,11 @@ const Reportable = Super => class extends Super {
     return new Promise((resolve, reject) => {
       this.db
         .find({ protocolId, 'data.edges': { $exists: true } })
-        .projection({ 'data.edges.__countable__': 1, _id: 0 })
-        .exec(resolveOrReject(resolveWithDocCount(resolve), reject));
+        .projection({ [`data.edges.${pseudoField}`]: 1, _id: 0 })
+        .exec(resolveOrReject(docs => resolve(docs.reduce((sum, doc) => {
+          sum += doc.data.edges[pseudoField].length;
+          return sum;
+        }, 0)), reject));
     });
   }
 };
