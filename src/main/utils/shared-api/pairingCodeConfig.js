629733c35f27c11983532637d16d46e1bea7e03f
Bryan Fox <git@bryfox.com>
2018-09-04 19:37:54 -0400

    Replace shared-api with submodule ref

--- src/main/utils/shared-api/pairingCodeConfig.js
+++
@@ -1,8 +0,0 @@
-const characters = 'abcdefghijklmnopqrstuvwxyz';
-const CharacterSet = Object.freeze(characters.split(''));
-const PairingCodeLength = 16;
-
-module.exports = {
-  CharacterSet,
-  PairingCodeLength,
-};
