d34a48b1b4479ff309bdf96868eb57d0ba01fd22
Bryan Fox <git@bryfox.com>
2018-09-07 17:21:40 -0400

    Add hot module reloading

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
-
 import { Provider } from 'react-redux';
+import { hot } from 'react-hot-loader';
 
 // Styles must be imported before components to support css prop caching on import
 import './styles/main.scss';
@@ -10,15 +10,12 @@ import './styles/main.scss';
 import { store } from './ducks/store';
 import App from './containers/App';
 
-const startApp = () => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </Provider>,
-    document.getElementById('root'),
-  );
-};
+const ConnectedApp = hot(module)(() => (
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
+));
 
-startApp();
+ReactDOM.render(<ConnectedApp />, document.getElementById('root'));
