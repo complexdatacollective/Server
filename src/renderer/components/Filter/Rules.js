9a8e99fa1f59209274f6c649127dc2c5d67085c1
Bryan Fox <git@bryfox.com>
2018-11-29 11:53:59 -0500

    Add transitions from Architect
    - Appear is required for FilterGroup
    - Drawer can be used for expanding export options

--- src/renderer/components/Filter/Rules.js
+++ src/renderer/components/Filter/Rules.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import { SortableContainer } from 'react-sortable-hoc';
-// import { TransitionGroup } from 'react-transition-group';
+import { TransitionGroup } from 'react-transition-group';
 import Rule from './Rule';
-// import AppearTransition from '../Transitions/Appear';
+import AppearTransition from '../Transitions/Appear';
 
 const Rules = SortableContainer(
   ({ rules, onUpdateRule, onDeleteRule, variableRegistry }) => (
-    <div className="rules">
-      {/* <TransitionGroup className="rules"> */}
+    <TransitionGroup className="rules">
       {rules.map((rule, index) => (
-        // <AppearTransition
-        //   key={`rule-${rule.id}`}
-        // >
-        <Rule
-          {...rule}
+        <AppearTransition
           key={`rule-${rule.id}`}
-          index={index}
-          sortIndex={index}
-          onUpdateRule={onUpdateRule}
-          onDeleteRule={onDeleteRule}
-          className="rules__rule"
-          variableRegistry={variableRegistry}
-        />
-        // </AppearTransition>
+        >
+          <Rule
+            {...rule}
+            index={index}
+            sortIndex={index}
+            onUpdateRule={onUpdateRule}
+            onDeleteRule={onDeleteRule}
+            className="rules__rule"
+            variableRegistry={variableRegistry}
+          />
+        </AppearTransition>
       ))}
-      {/* </TransitionGroup> */}
-    </div>
+    </TransitionGroup>
   ),
 );
 
