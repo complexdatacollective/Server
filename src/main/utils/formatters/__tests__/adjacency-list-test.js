422ac54fcbe70c8144a6cb8e6b8f40f294844245
Bryan Fox <git@bryfox.com>
2018-12-04 11:50:06 -0500

    Refactor stream use in tests

--- src/main/utils/formatters/__tests__/adjacency-list-test.js
+++ src/main/utils/formatters/__tests__/adjacency-list-test.js
@@ -1,5 +1,5 @@
 /* eslint-env jest */
-import { Writable } from 'stream';
+import { makeWriteableStream } from '../../../../../config/jest/setupTestEnv';
 import { asAdjacencyList, toCSVStream } from '../adjacency-list';
 
 describe('asAdjacencyList', () => {
@@ -17,56 +17,44 @@ describe('asAdjacencyList', () => {
 });
 
 describe('toCSVStream', () => {
-  let chunks;
-  let streamToString;
   let writable;
 
   beforeEach(() => {
-    chunks = [];
-    streamToString = async stream => new Promise((resolve, reject) => {
-      stream.on('finish', () => { resolve(chunks.join('')); });
-      stream.on('error', (err) => { reject(err); });
-    });
-    writable = new Writable({
-      write(chunk, encoding, next) {
-        chunks.push(chunk.toString());
-        next(null);
-      },
-    });
+    writable = makeWriteableStream();
   });
 
   it('Writes a simple csv', async () => {
     const list = asAdjacencyList([{ from: 1, to: 2 }]);
     toCSVStream(list, writable);
-    const csv = await streamToString(writable);
+    const csv = await writable.asString();
     expect(csv).toEqual('1,2\r\n2,1\r\n');
   });
 
   it('Writes multiple edges', async () => {
     const list = asAdjacencyList([{ from: 1, to: 2 }, { from: 1, to: 3 }]);
     toCSVStream(list, writable);
-    const csv = await streamToString(writable);
+    const csv = await writable.asString();
     expect(csv).toEqual('1,2,3\r\n2,1\r\n3,1\r\n');
   });
 
   it('Writes a csv for directed edges', async () => {
     const list = asAdjacencyList([{ from: 1, to: 2 }, { from: 1, to: 3 }], true);
     toCSVStream(list, writable);
-    const csv = await streamToString(writable);
+    const csv = await writable.asString();
     expect(csv).toEqual('1,2,3\r\n');
   });
 
   it('Writes a csv for directed edges (inverse)', async () => {
     const list = asAdjacencyList([{ from: 1, to: 2 }, { from: 3, to: 1 }], true);
     toCSVStream(list, writable);
-    const csv = await streamToString(writable);
+    const csv = await writable.asString();
     expect(csv).toEqual('1,2\r\n3,1\r\n');
   });
 
   it('Ignores duplicate edges', async () => {
     const list = asAdjacencyList([{ from: 1, to: 2 }, { from: 1, to: 2 }]);
     toCSVStream(list, writable);
-    const csv = await streamToString(writable);
+    const csv = await writable.asString();
     expect(csv).toEqual('1,2\r\n2,1\r\n');
   });
 });
