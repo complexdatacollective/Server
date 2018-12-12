cf3c2f5589996a9119420279a7eb1b98fd48223f
Bryan Fox <git@bryfox.com>
2018-12-12 12:23:38 -0500

    Add filter & query steps to export backend

--- src/main/utils/formatters/__tests__/network-test.js
+++ src/main/utils/formatters/__tests__/network-test.js
@@ -1,7 +1,45 @@
 /* eslint-env jest */
-const { getNodeAttributes, nodeAttributesProperty, unionOfNetworks } = require('../network');
+const {
+  filterNetworkEntities,
+  filterNetworksWithQuery,
+  getNodeAttributes,
+  nodeAttributesProperty,
+  unionOfNetworks,
+} = require('../network');
 
 describe('network format helpers', () => {
+  describe('filtering & querying', () => {
+    const ruleConfig = {
+      rules: [
+        {
+          type: 'alter',
+          options: { type: 'person', operator: 'EXACTLY', attribute: 'age', value: 20 },
+          assert: { operator: 'GREATER_THAN', value: 0 }, // for query only
+        },
+      ],
+      join: 'OR',
+    };
+
+    describe('filterNetworksWithQuery()', () => {
+      it('includes nodes from attribute query', () => {
+        const a = { nodes: [{ type: 'person', [nodeAttributesProperty]: { age: 20 } }], edges: [] };
+        const b = { nodes: [{ type: 'person', [nodeAttributesProperty]: { age: 20 } }], edges: [] };
+        const c = { nodes: [{ type: 'person', [nodeAttributesProperty]: { age: 21 } }], edges: [] };
+        const networks = [a, b, c];
+        expect(filterNetworksWithQuery(networks, ruleConfig)).toEqual([a, b]);
+      });
+    });
+
+    describe('filterNetworkEntities()', () => {
+      it('includes nodes matching attributes', () => {
+        const alice = { type: 'person', [nodeAttributesProperty]: { age: 20 } };
+        const bob = { type: 'person', [nodeAttributesProperty]: { age: 21 } };
+        const networks = [{ nodes: [alice, bob], edges: [] }];
+        expect(filterNetworkEntities(networks, ruleConfig)[0].nodes).toEqual([alice]);
+      });
+    });
+  });
+
   describe('unionOfNetworks', () => {
     it('joins nodes of two networks', () => {
       const a = { nodes: [{ id: 1 }], edges: [] };
--- src/main/utils/formatters/network.js
+++ src/main/utils/formatters/network.js
@@ -1,3 +1,6 @@
+const getQuery = require('../network-query/query').default;
+const getFilter = require('../network-query/filter').default;
+
 // TODO: share with other places this is defined
 const nodePrimaryKeyProperty = '_uid';
 
@@ -12,7 +15,33 @@ const unionOfNetworks = networks =>
     return union;
   }, { nodes: [], edges: [] });
 
+/**
+ * Run the query on each network; filter for those which meet the criteria (i.e., where the query
+ * evaluates to `true`).
+ * @param  {Object[]} networks An array of NC networks
+ * @param  {Object} inclusionQueryConfig a query definition with asserting rules
+ * @return {Object[]} a subset of the networks
+ */
+const filterNetworksWithQuery = (networks, inclusionQueryConfig) =>
+  (inclusionQueryConfig ? networks.filter(getQuery(inclusionQueryConfig)) : networks);
+
+/**
+ * Filter each network based on the filter config.
+ * @param  {Object[]} networks An array of NC networks
+ * @param  {Object} filterConfig a filter definition with rules
+ * @return {Object[]} a copy of `networks`, each possibly containing a subset of the original
+ */
+const filterNetworkEntities = (networks, filterConfig) => {
+  if (!filterConfig) {
+    return networks;
+  }
+  const filter = getFilter(filterConfig);
+  return networks.map(network => filter(network));
+};
+
 module.exports = {
+  filterNetworkEntities,
+  filterNetworksWithQuery,
   getNodeAttributes,
   nodeAttributesProperty,
   nodePrimaryKeyProperty,
