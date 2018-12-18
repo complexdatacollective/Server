c6f9ee830125f718b7c440386b3074ba2a963310
Bryan Fox <git@bryfox.com>
2018-12-18 13:42:39 -0500

    Bump version for alpha 10

--- src/renderer/utils/appVersion.js
+++ src/renderer/utils/appVersion.js
@@ -6,6 +6,7 @@ const codenames = {
   '4.0.0-alpha.4': 'Gold-Bug',
   '4.0.0-alpha.8': 'Lochs & Glens',
   '4.0.0-alpha.9': 'Arrakis',
+  '4.0.0-alpha.10': 'Porthleven',
 };
 
 const appVersion = remote.app.getVersion();
