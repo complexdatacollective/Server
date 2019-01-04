cb07316ff7b636f34a7d67020f4b19520c5b153f
Bryan Fox <git@bryfox.com>
2019-01-04 14:24:05 -0500

    Format and style counts
    Based on feedback at #223

--- src/renderer/utils/formatters.js
+++ src/renderer/utils/formatters.js
@@ -1,11 +1,13 @@
 const dateOpts = { year: '2-digit', month: 'numeric', day: 'numeric' };
 
-const decimalFormatter = new Intl.NumberFormat();
+const decimalFormatter = new Intl.NumberFormat(undefined, { maximumFractionDigits: 2 });
 
 const formatDate = d => (d ? d.toLocaleString(undefined, dateOpts) : '');
 const formatDecimal = n => (isNaN(n) ? '' : decimalFormatter.format(n));
+const formatNumber = n => (Number.isInteger(n) ? n.toString() : formatDecimal(n));
 
 export {
   formatDate,
   formatDecimal,
+  formatNumber,
 };
