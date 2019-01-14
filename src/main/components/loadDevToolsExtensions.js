9252c6dff87fe2ba5b528f71d5cc6d00a97469ca
Bryan Fox <git@bryfox.com>
2019-01-14 13:16:57 -0500

    Refactor dev tools extension

---
+++ src/main/components/loadDevToolsExtensions.js
@@ -0,0 +1,20 @@
+const { BrowserWindow } = require('electron');
+
+const loadDevToolsExtensions = () => {
+  const extensions = process.env.NC_DEVTOOLS_EXENSION_PATH;
+  if (process.env.NODE_ENV !== 'development' || !extensions) {
+    return;
+  }
+  try {
+    extensions.split(';').forEach(filepath => BrowserWindow.addDevToolsExtension(filepath));
+  } catch (err) {
+    /* eslint-disable no-console, global-require */
+    const chalk = require('chalk');
+    console.warn(err);
+    console.warn(chalk.yellow('A Chrome dev tools extension failed to load. If the extension has upgraded, update your NC_DEVTOOLS_EXENSION_PATH:'));
+    console.warn(chalk.yellow(process.env.NC_DEVTOOLS_EXENSION_PATH));
+    /* eslint-enable */
+  }
+};
+
+module.exports = loadDevToolsExtensions;
--- src/main/components/mainWindow.js
+++ src/main/components/mainWindow.js
@@ -2,24 +2,9 @@ const path = require('path');
 const url = require('url');
 const { BrowserWindow } = require('electron');
 
-const DefaultHomeRoute = '/overview';
+const loadDevToolsExtensions = require('./loadDevToolsExtensions');
 
-const loadDevToolsExtensions = () => {
-  const extensions = process.env.NC_DEVTOOLS_EXENSION_PATH;
-  if (process.env.NODE_ENV !== 'development' || !extensions) {
-    return;
-  }
-  try {
-    extensions.split(';').forEach(filepath => BrowserWindow.addDevToolsExtension(filepath));
-  } catch (err) {
-    /* eslint-disable no-console, global-require */
-    const chalk = require('chalk');
-    console.warn(err);
-    console.warn(chalk.yellow('A Chrome dev tools extension failed to load. If the extension has upgraded, update your NC_DEVTOOLS_EXENSION_PATH:'));
-    console.warn(chalk.yellow(process.env.NC_DEVTOOLS_EXENSION_PATH));
-    /* eslint-enable */
-  }
-};
+const DefaultHomeRoute = '/overview';
 
 /**
  * Convert an app route to a full URL using hash-based routing.
