00994a796d0308366e73167590e613305807a566
Rebecca Madsen <sollasollew@outlook.com>
2019-03-21 12:26:29 -0400

    add ego and attributes to edge csv

--- src/main/utils/formatters/edge-list.js
+++ src/main/utils/formatters/edge-list.js
@@ -1,6 +1,7 @@
 const { Readable } = require('stream');
 
-const { csvEOL } = require('./csv');
+const { cellValue, csvEOL } = require('./csv');
+const { nodePrimaryKeyProperty } = require('./network');
 
 /**
  * Builds an edge list for a network, based only on its edges (it need
@@ -20,18 +21,42 @@ const { csvEOL } = require('./csv');
  * @param  {Object} network NC network containing edges
  * @param  {Boolean} directed if false, adjacencies are represented in both directions
  *                            default: false
- * @return {Object.<string, Set>} the adjacency list
+ * @return {Array} the edges list
  */
-const asEdgeList = (network, directed = false) =>
-  (network.edges || []).reduce((acc, val) => {
-    acc[val.from] = acc[val.from] || new Set();
-    acc[val.from].add(val.to);
-    if (directed === false) {
-      acc[val.to] = acc[val.to] || new Set();
-      acc[val.to].add(val.from);
-    }
-    return acc;
-  }, {});
+const asEdgeList = (network, directed = false) => {
+  if (directed === false) {
+    // this may change if we have support for directed vs undirected edges in NC
+    return network.edges.reduce((arr, edge) => (
+      arr.concat(
+        { ...edge, _source: edge.from, _target: edge.to },
+        { ...edge, _source: edge.to, _target: edge.from },
+      )
+    ), []);
+  }
+  return network.edges.map(edge => ({ ...edge, _source: edge.from, _target: edge.to }));
+};
+
+/**
+ * The output of this formatter will contain the primary key (_uid)
+ * and all model data (inside the `attributes` property)
+ */
+const attributeHeaders = (edges, withEgo) => {
+  const initialHeaderSet = new Set([]);
+  if (withEgo) {
+    initialHeaderSet.add('_egoID');
+  }
+  initialHeaderSet.add(nodePrimaryKeyProperty);
+  initialHeaderSet.add('_source');
+  initialHeaderSet.add('_target');
+
+  const headerSet = edges.reduce((headers, edge) => {
+    Object.keys(edge.attributes).forEach((key) => {
+      headers.add(key);
+    });
+    return headers;
+  }, initialHeaderSet);
+  return [...headerSet];
+};
 
 /**
  * Write a CSV reprensentation of the list to the given Writable stream.
@@ -46,19 +71,34 @@ const asEdgeList = (network, directed = false) =>
  *
  * @return {Object} an abort controller; call the attached abort() method as needed.
  */
-// TODO: quoting/escaping (not needed while we're only using UUIDs)
-const toCSVStream = (edgeList, outStream) => {
-  const adjacencies = Object.entries(edgeList);
-  const totalChunks = adjacencies.length;
+const toCSVStream = (edges, withEgo, outStream) => {
+  const totalChunks = edges.length;
   let chunkContent;
   let chunkIndex = 0;
+  const attrNames = attributeHeaders(edges, withEgo);
+  let headerWritten = false;
+  let edge;
 
   const inStream = new Readable({
     read(/* size */) {
-      if (chunkIndex < totalChunks) {
-        const [fromId, destinations] = adjacencies[chunkIndex];
-        chunkContent = [...destinations].map(toId => `${fromId},${toId}`).join(csvEOL);
-        this.push(`${chunkContent}${csvEOL}`);
+      if (!headerWritten) {
+        this.push(`${attrNames.map(attr => cellValue(attr)).join(',')}${csvEOL}`);
+        headerWritten = true;
+      } else if (chunkIndex < totalChunks) {
+        edge = edges[chunkIndex];
+        const values = attrNames.map((attrName) => {
+          // primary key/ego id/to/from exist at the top-level; all others inside `.attributes`
+          let value;
+          if (attrName === nodePrimaryKeyProperty || attrName === '_egoID' ||
+            attrName === '_source' || attrName === '_target') {
+            value = edge[attrName];
+          } else {
+            value = edge.attributes[attrName];
+          }
+          return cellValue(value);
+        });
+        chunkContent = `${values.join(',')}${csvEOL}`;
+        this.push(chunkContent);
         chunkIndex += 1;
       } else {
         this.push(null);
@@ -75,11 +115,12 @@ const toCSVStream = (edgeList, outStream) => {
 };
 
 class EdgeListFormatter {
-  constructor(data, directed = false) {
-    this.list = asEdgeList(data, directed);
+  constructor(data, directed = false, includeEgo = false) {
+    this.list = asEdgeList(data, directed, includeEgo);
+    this.includeEgo = includeEgo;
   }
   writeToStream(outStream) {
-    return toCSVStream(this.list, outStream);
+    return toCSVStream(this.list, this.includeEgo, outStream);
   }
 }
 
