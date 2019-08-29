d25d773baccc5702df3c03f9d01f2d85fd4dad0e
Rebecca Madsen <sollasollew@outlook.com>
2019-08-29 10:53:35 -0400

    mock localStorage

--- src/renderer/ducks/__tests__/store-test.js
+++ src/renderer/ducks/__tests__/store-test.js
@@ -2,6 +2,8 @@
 const store = require('../store');
 const rootReducer = require('../modules/rootReducer');
 
+require('../../__mocks__/localStorageMock');
+
 jest.mock('electron-log');
 jest.mock('../modules/rootReducer');
 jest.mock('../middleware/logger', () => (/* store */) => (/* next */) => jest.fn());
