e5e35c8bebf2e63b41977e59796392d2b70b2732
Joshua Melville <joshua.melville@northwestern.edu>
2020-01-07 15:09:32 +0200

    update tests

--- src/main/server/__tests__/certificateManager-test.js
+++ src/main/server/__tests__/certificateManager-test.js
@@ -1,7 +1,7 @@
 /* eslint-env jest */
 const selfsigned = require('selfsigned');
 
-const { ensurePemKeyPair } = require('../ensurePemKeyPair');
+const { ensurePemKeyPair } = require('../certificateManager');
 const promisedFs = require('../../utils/promised-fs');
 
 jest.mock('selfsigned');
