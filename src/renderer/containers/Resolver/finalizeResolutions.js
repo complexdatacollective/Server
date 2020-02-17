4643197a8aa730aa754475cb67aa712157c30290
Steve Mckellar <steve@wwqrd.uk>
2020-02-17 18:11:43 +0000

    Resolution finalizer WIP

--- src/renderer/containers/Resolver/finalizeResolutions.js
+++ src/renderer/containers/Resolver/finalizeResolutions.js
@@ -2,11 +2,21 @@
 
 // [1, 2=
 
-const finializeResolutions = ({ resolutions }) => {
-  resolutions.revers()
+const finializeResolutions = ({ resolutions, ...rest }) => {
+  const result = resolutions.reverse()
     .reduce((acc, resolution) => {
+      // if any of the nodes in this resolution are accounted for, this resolution is complete
+      if (resolution.nodes.some(node => acc.nodes.includes(node))) { return acc; }
+      return {
+        nodes: [...acc.nodes, ...resolution.nodes],
+        resolutions: [...acc.resolutions, resolution],
+      };
+    }, { nodes: [], resolutions: [] });
 
-    }, []);
+  return {
+    ...rest,
+    resolutions: result.resolutions,
+  };
 };
 
 export default finializeResolutions;
