9af5efb53cbb56560cc5eb39bf160cf6393a1b28
Steve Mckellar <steve@wwqrd.uk>
2020-03-10 15:36:29 +0000

    Reorganise ResolverManager sub components so that they aren't all in one file

--- src/main/data-managers/ResolverManager.js
+++ src/main/data-managers/ResolverManager.js
@@ -1,111 +1,16 @@
 /* eslint-disable no-underscore-dangle */
 
 const path = require('path');
-const split = require('split');
-const miss = require('mississippi');
-const { DateTime } = require('luxon');
-const { get, find, findIndex, reduce } = require('lodash');
-const csvToJson = require('../utils/streams/csvToJson');
-const { convertUuidToDecimal, nodePrimaryKeyProperty, nodeAttributesProperty } = require('../utils/formatters/network');
-const ResolverDB = require('./ResolverDB');
-const SessionDB = require('./SessionDB');
-const commandRunner = require('../utils/commandRunner');
 const { RequestError, ErrorMessages } = require('../errors/RequestError');
 const {
   insertEgoInNetworks,
   transposedCodebook,
   unionOfNetworks,
 } = require('../utils/formatters/network');
-const {
-  AttributeListFormatter,
-} = require('../utils/formatters/index');
-
-/**
- * nodes are transmitted to the resolver as numerical ids generated
- * by convertUuidToDecimal, this find method handles that additional
- * step.
- */
-const findNode = findId =>
-  (node) => {
-    const id = convertUuidToDecimal(node[nodePrimaryKeyProperty]);
-    return id === findId;
-  };
-
-const getNode = (network, id) => {
-  const node = network.nodes.find(findNode(id));
-
-  if (!node) {
-    throw new Error(
-      `Corresponding node data for '${id}' (convertUuidToDecimal) could not be found in network object.`
-    );
-  }
-
-  return {
-    [nodePrimaryKeyProperty]: node[nodePrimaryKeyProperty],
-    [nodeAttributesProperty]: node[nodeAttributesProperty],
-  };
-};
-
-/**
- * Given a match which contains ids and a probability, append
- * network data for that node and parse the probabily as a float.
- *
- * { networkCanvasAlterID_1, networkCanvasAlterID_2, prob: '0.0' } ->
- *   { nodes: [ { id, attributes }, { id, attributes } ], prob: 0.0 }
- */
-const appendNodeNetworkData = network =>
-  miss.through((chunk, encoding, callback) => {
-    try {
-      const obj = JSON.parse(chunk);
-
-      const nodes = [
-        getNode(network, obj.networkCanvasAlterID_1),
-        getNode(network, obj.networkCanvasAlterID_2),
-      ];
-
-      const prob = parseFloat(obj.prob);
-
-      const result = JSON.stringify({
-        nodes,
-        prob,
-      });
-
-      callback(null, result);
-    } catch (err) {
-      callback(err);
-    }
-  });
-
-/**
- * Create a fully formed pipeline that emits each match as a json
- * chunk including original network data for each pair:
- * `{ nodes: [ { id, attributes }, { id, attributes } ], prob: 0.0 }`
- *
- * Because this is a 'pipeline' (https://github.com/maxogden/mississippi#pipeline)
- * if any of the streams emits an error, all the streams in the pipeline
- * will be closed automatically, and the error can be captured on the last
- * stream with `.on('error', () => {});`
- */
-const getNetworkResolver = ({
-  command,
-  codebook,
-} = {}) =>
-  (network) => {
-    const formatter = new AttributeListFormatter(network, false, false, codebook);
-
-    return commandRunner(command)
-      .then((resolver) => {
-        const pipeline = miss.pipeline(
-          resolver(),
-          split(),
-          csvToJson(),
-          appendNodeNetworkData(network),
-        );
-
-        formatter.writeToStream(pipeline);
-        return pipeline;
-      });
-  };
+const { getNetworkResolver } = require('../utils/getNetworkResolver');
+const { transformSessions } = require('../utils/transformSessions');
+const ResolverDB = require('./ResolverDB');
+const SessionDB = require('./SessionDB');
 
 const formatResolution = resolution => ({
   ...resolution,
@@ -117,105 +22,6 @@ const formatSession = (session) => {
   const caseID = session && session.data && session.data.sessionVariables &&
     session.data.sessionVariables._caseID;
   return { ...session.data, _id: id, _caseID: caseID };
-};
-
-const getPriorResolutions = (resolutions, resolutionId) => {
-  // from oldest to newest
-  const sortedResolutions = [...resolutions]
-    .sort((a, b) => DateTime.fromISO(a.date) - DateTime.fromISO(b.date));
-
-
-  if (!resolutionId) { return sortedResolutions; }
-
-  const lastResolution = findIndex(sortedResolutions, ['id', resolutionId]);
-
-  if (lastResolution === -1) {
-    throw new Error(`Resolution "${resolutionId}" could not be found`);
-  }
-
-  return sortedResolutions.slice(0, lastResolution + 1);
-};
-
-// 1. chunk sessions by resolutions
-// Maybe resolutions should include references to included sessions rather than
-// calculating it?
-const getSessionsByResolution = (resolutions, sessions) =>
-  sessions.reduce((memo, session) => {
-    const sessionDate = DateTime.fromISO(session.date);
-
-    const resolution = find(
-      resolutions,
-      ({ date }) =>
-        sessionDate < DateTime.fromISO(date),
-    );
-
-    const resolutionId = (resolution && resolution.id) || '_unresolved';
-
-    const group = get(memo, [resolutionId], []);
-
-    return {
-      ...memo,
-      [resolutionId]: [...group, session],
-    };
-  }, {});
-
-// TODO: make sure this still contains _egoID for 'new' nodes
-const applyTransform = (network, transform) => {
-  const withTransformNodesRemoved = network.nodes.filter(
-    node => !transform.nodes.includes(node[nodePrimaryKeyProperty]),
-  );
-
-  // nodes weren't found return original network
-  if (withTransformNodesRemoved.length !== network.nodes.length - transform.nodes.length) {
-    return network;
-  }
-
-  const nodes = withTransformNodesRemoved.concat([{
-    [nodePrimaryKeyProperty]: transform.id,
-    [nodeAttributesProperty]: transform.attributes,
-  }]);
-
-  const edges = network.edges.map(edge => ({
-    ...edge,
-    from: transform.nodes.includes(edge.from) ? transform.id : edge.from,
-    to: transform.nodes.includes(edge.to) ? transform.id : edge.to,
-  }));
-
-  return {
-    ...network,
-    nodes,
-    edges,
-  };
-};
-
-const transformSessions = (sessions, resolutions, { useEgoData, fromResolution }) => {
-  const priorResolutions = getPriorResolutions(resolutions, fromResolution);
-  const sessionsByResolution = getSessionsByResolution(priorResolutions, sessions);
-
-  // For each resolution merge sessions and apply resolution
-  const resultNetwork = reduce(
-    priorResolutions,
-    (accNetwork, resolution) => {
-      const sessionNetworks = sessionsByResolution[resolution.id]; // array of networks
-
-      if (!sessionNetworks) {
-        // if no new sessions, operate on existing
-        return resolution.transforms.reduce(applyTransform, accNetwork);
-      }
-
-      const withEgoData = useEgoData ? insertEgoInNetworks(sessionNetworks) : sessionNetworks;
-      const unifiedNetwork = unionOfNetworks([accNetwork, ...withEgoData]);
-
-      return resolution.transforms.reduce(applyTransform, unifiedNetwork);
-    },
-    { nodes: [], edges: [], ego: [] },
-  );
-
-  if (sessionsByResolution._unresolved) {
-    return unionOfNetworks([resultNetwork, ...sessionsByResolution._unresolved]);
-  }
-
-  return resultNetwork;
 };
 
 /**
@@ -236,7 +42,10 @@ class ResolverManager {
     const {
       enableEntityResolution,
       resolutionId,
-    } = entityResolutionOptions;
+    } = entityResolutionOptions || {
+      enableEntityResolution: false,
+      resolutionId: null,
+    };
 
     const protocolId = protocol._id;
 
@@ -292,10 +101,6 @@ class ResolverManager {
 }
 
 module.exports = {
-  getPriorResolutions,
-  getSessionsByResolution,
-  applyTransform,
-  transformSessions,
   ResolverManager,
   default: ResolverManager,
 };
---
+++ src/main/utils/getNetworkResolver.js
@@ -0,0 +1,102 @@
+/* eslint-disable no-underscore-dangle */
+
+const split = require('split');
+const miss = require('mississippi');
+const csvToJson = require('./streams/csvToJson');
+const { convertUuidToDecimal, nodePrimaryKeyProperty, nodeAttributesProperty } = require('./formatters/network');
+const commandRunner = require('./commandRunner');
+const {
+  AttributeListFormatter,
+} = require('./formatters/index');
+
+/**
+ * nodes are transmitted to the resolver as numerical ids generated
+ * by convertUuidToDecimal, this find method handles that additional
+ * step.
+ */
+const findNode = findId =>
+  (node) => {
+    const id = convertUuidToDecimal(node[nodePrimaryKeyProperty]);
+    return id === findId;
+  };
+
+const getNode = (network, id) => {
+  const node = network.nodes.find(findNode(id));
+
+  if (!node) {
+    throw new Error(
+      `Corresponding node data for '${id}' (convertUuidToDecimal) could not be found in network object.`
+    );
+  }
+
+  return {
+    [nodePrimaryKeyProperty]: node[nodePrimaryKeyProperty],
+    [nodeAttributesProperty]: node[nodeAttributesProperty],
+  };
+};
+
+/**
+ * Given a match which contains ids and a probability, append
+ * network data for that node and parse the probabily as a float.
+ *
+ * { networkCanvasAlterID_1, networkCanvasAlterID_2, prob: '0.0' } ->
+ *   { nodes: [ { id, attributes }, { id, attributes } ], prob: 0.0 }
+ */
+const appendNodeNetworkData = network =>
+  miss.through((chunk, encoding, callback) => {
+    try {
+      const obj = JSON.parse(chunk);
+
+      const nodes = [
+        getNode(network, obj.networkCanvasAlterID_1),
+        getNode(network, obj.networkCanvasAlterID_2),
+      ];
+
+      const prob = parseFloat(obj.prob);
+
+      const result = JSON.stringify({
+        nodes,
+        prob,
+      });
+
+      callback(null, result);
+    } catch (err) {
+      callback(err);
+    }
+  });
+
+/**
+ * Create a fully formed pipeline that emits each match as a json
+ * chunk including original network data for each pair:
+ * `{ nodes: [ { id, attributes }, { id, attributes } ], prob: 0.0 }`
+ *
+ * Because this is a 'pipeline' (https://github.com/maxogden/mississippi#pipeline)
+ * if any of the streams emits an error, all the streams in the pipeline
+ * will be closed automatically, and the error can be captured on the last
+ * stream with `.on('error', () => {});`
+ */
+const getNetworkResolver = ({
+  command,
+  codebook,
+} = {}) =>
+  (network) => {
+    const formatter = new AttributeListFormatter(network, false, false, codebook);
+
+    return commandRunner(command)
+      .then((resolver) => {
+        const pipeline = miss.pipeline(
+          resolver(),
+          split(),
+          csvToJson(),
+          appendNodeNetworkData(network),
+        );
+
+        formatter.writeToStream(pipeline);
+        return pipeline;
+      });
+  };
+
+module.exports = {
+  getNetworkResolver,
+  default: getNetworkResolver,
+};
