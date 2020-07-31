7fb02b198ef6f44780b88671f01a4761c2178169
Joshua Melville <joshua.melville@northwestern.edu>
2020-07-31 12:19:09 +0100

    update mockValue() and fix logic error in buildMockData

--- src/main/utils/buildMockData.js
+++ src/main/utils/buildMockData.js
@@ -8,21 +8,26 @@ const caseProperty = '_caseID';
 const mockCoord = () => faker.random.number({ min: 0, max: 1, precision: 0.000001 });
 
 // Todo: make these mock values reflect validation
-// Todo: include date time
-const mockValue = (nodeVariable) => {
-  switch (nodeVariable.type) {
+const mockValue = (variable) => {
+  switch (variable.type) {
     case 'boolean':
       return faker.random.boolean();
     case 'number':
       return faker.random.number({ min: 20, max: 100 });
+    case 'scalar':
+      return faker.random.number({ min: 0, max: 1, precision: 0.001 });
+    case 'datetime':
+      return faker.date.recent().toISOString().slice(0, 10);
     case 'ordinal':
-      return faker.random.arrayElement(nodeVariable.options).value;
+      return faker.random.arrayElement(variable.options).value;
     case 'categorical':
-      return [faker.random.arrayElement(nodeVariable.options).value];
+      return [faker.random.arrayElement(variable.options).value];
     case 'layout':
       return { x: mockCoord(), y: mockCoord() };
     default: {
-      if (nodeVariable.name === 'name') {
+      console.log('testing for variable name', variable.name);
+      if (variable.name.toLowerCase() === 'name' || variable.name.toLowerCase().includes('name')) {
+        console.log(variable.name, 'is a name');
         return faker.name.findName();
       }
       return faker.random.word();
@@ -81,7 +86,7 @@ const buildMockData = (
     const networkMaxEdges = 20;
     const networkMinEdges = 1;
     // eslint-disable-next-line no-bitwise
-    const pickNodeUid = () => nodes[~~(Math.random() * nodes.length)][nodePrimaryKeyProperty];
+    const pickNodeUid = () => nodes[~~(Math.random() * (nodes.length - 1))][nodePrimaryKeyProperty];
 
     codebookEdgeTypes.forEach((edgeType) => {
       const edgesOfThisType =
