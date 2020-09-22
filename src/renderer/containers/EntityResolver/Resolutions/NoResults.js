57cb32fb1a5b2529599396f992b6b9f7fbc7d65b
Steve Mckellar <steve@wwqrd.uk>
2020-09-22 13:35:20 +0100

    Explain export on NoResults screen

--- src/renderer/containers/EntityResolver/Resolutions/NoResults.js
+++ src/renderer/containers/EntityResolver/Resolutions/NoResults.js
@@ -3,8 +3,8 @@ import React from 'react';
 const NoResults = () => (
   <React.Fragment>
     <p>The entity resolution script did not return any results.</p>
-    <p>This does not necessarily indicate a failure, it may be that
-      no matching nodes were found by the script.</p>
+    <p>To continue with export, please close this window and either select
+      a previous resolution or uncheck &quot;entity resolution&quot;</p>
   </React.Fragment>
 );
 
