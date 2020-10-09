51a62aae7023d3749968c11496bd6e01b42402b1
Steve Mckellar <steve@wwqrd.uk>
2020-10-09 17:18:57 +0100

    Delete old export code

--- src/main/utils/formatters/index.js
+++ src/main/utils/formatters/index.js
@@ -1,13 +1,5 @@
-const GraphMLFormatter = require('../network-exporters/graphml/GraphMLFormatter');
 const { AdjacencyMatrixFormatter } = require('./matrix');
-const { AttributeListFormatter } = require('./attribute-list');
-const { EgoListFormatter } = require('./ego-list');
-const { EdgeListFormatter } = require('./edge-list');
 
 module.exports = {
   AdjacencyMatrixFormatter,
-  AttributeListFormatter,
-  EgoListFormatter,
-  EdgeListFormatter,
-  GraphMLFormatter,
 };
