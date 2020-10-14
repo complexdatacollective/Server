4660aad55c7631af7ccb69258b3ab61170985fd8
Steve Mckellar <steve@wwqrd.uk>
2020-10-14 11:44:56 +0100

    Add logging info for version mismatch

--- src/main/server/devices/versionGatekeeper.js
+++ src/main/server/devices/versionGatekeeper.js
@@ -1,8 +1,14 @@
+const logger = require('electron-log');
 const SERVER_API_VERSION = '1';
 
 const versionGatekeeper = (req, res, next) => {
   const deviceApiVersion = req.header('X-Device-API-Version', 'Not specified');
   if (deviceApiVersion !== SERVER_API_VERSION) {
+    logger.warn('[DeviceAPI]', {
+      error: 'X-Device-API-Version mismatch.',
+      server: SERVER_API_VERSION,
+      device: deviceApiVersion,
+    });
     res.send(
       400,
       {
