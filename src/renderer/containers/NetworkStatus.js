8b3ebc53e9ac9a9a109c52074cdd15d2dd3b9e86
Steve Mckellar <steve@wwqrd.uk>
2020-10-29 15:12:19 +0000

    Get network status from API

--- src/renderer/containers/NetworkStatus.js
+++ src/renderer/containers/NetworkStatus.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import AdminApiClient from '../utils/adminApiClient';
 
 const buttonClass = 'device-icon';
 
-const NetworkStatus = ({ serverStatus }) => {
+const getMdnsStatus = ({ isAdvertising, mdnsIsSupported }) => {
+  if (!mdnsIsSupported) { return 'Unsupported'; }
+  return isAdvertising ? 'Active' : 'Pending';
+};
+
+const initialState = {};
+
+const NetworkStatus = () => {
+  const apiClient = useRef(new AdminApiClient());
+  const [state, setState] = useState(initialState);
+
+  useEffect(() => {
+    apiClient.current.get('/health')
+      .then((resp) => {
+        const ip = resp.serverStatus.ip && resp.serverStatus.ip.address;
+
+        setState({
+          hostname: resp.serverStatus.hostname,
+          ip: ip && ip.address,
+          deviceApiPort: resp.serverStatus.hostname,
+          mdnsStatus: getMdnsStatus(resp.serverStatus),
+          publicAddresses: resp.serverStatus.publicAddresses,
+          uptime: resp.serverStatus.uptime,
+        });
+      })
+      .catch(() => {
+        setState({});
+      });
+  }, []);
+
   return (
     <button className={buttonClass}>
       <span className="status-icon__badge" />
