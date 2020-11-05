8b427eb5615063a5ff179b98ad5c2a3cf25cf144
Steve Mckellar <steve@wwqrd.uk>
2020-11-05 18:59:36 +0000

    Add pending status for network status

--- src/renderer/containers/NetworkStatus.js
+++ src/renderer/containers/NetworkStatus.js
@@ -4,6 +4,12 @@ import InfoWindow from '../components/InfoWindow';
 import NetworkStatusTable from '../components/NetworkStatusTable';
 import useNetworkStatus from '../hooks/useNetworkStatus';
 
+const getNetworkIndicatorStatus = (networkStatus) => {
+  if (networkStatus.uptime === 0) { return 'error'; }
+  if (networkStatus.mdnsIsSupported && networkStatus.isAdvertising) { return 'ok'; }
+  return 'pending';
+};
+
 const NetworkStatus = () => {
   const networkStatus = useNetworkStatus();
   const [showNetworkModal, setShowNetworkModal] = useState(false);
@@ -13,6 +19,8 @@ const NetworkStatus = () => {
     { 'network-status--is-active': !!networkStatus.uptime },
   );
 
+  const networkIndicatorStatus = getNetworkIndicatorStatus(networkStatus);
+
   return [
     <button
       className={networkStatusClasses}
@@ -20,7 +28,7 @@ const NetworkStatus = () => {
       key="button"
     >
       <div className="network-status__icon">
-        <span className="network-status__badge" />
+        <span className={`network-status__badge network-status-badge--${networkIndicatorStatus}`} />
       </div>
 
       Network
