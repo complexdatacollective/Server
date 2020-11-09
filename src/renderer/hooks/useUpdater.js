1844867078c56045f93b0a7786eeb5ede7bc2137
Joshua Melville <joshua.melville@northwestern.edu>
2020-11-09 14:37:08 +0000

    implement useUpdater tests

--- src/renderer/hooks/useUpdater.js
+++ src/renderer/hooks/useUpdater.js
@@ -18,11 +18,11 @@ const renderers = {
   link: ({ children, href }) => <ExternalLink href={href}>{children}</ExternalLink>,
 };
 
-const getPlatformSpecificContent = (assets) => {
+export const getPlatformSpecificContent = (assets) => {
   if (!assets || assets.length === 0) {
     return {
       buttonText: 'Open Download Page',
-      onClickHandler: () => openExternalLink('https://networkcanvas.com/download.html'),
+      buttonLink: 'https://networkcanvas.com/download.html',
     };
   }
 
@@ -31,7 +31,7 @@ const getPlatformSpecificContent = (assets) => {
     const dmg = find(assets, value => value.name.split('.').pop() === 'dmg');
     return {
       buttonText: 'Download Installer',
-      onClickHandler: () => openExternalLink(dmg.browser_download_url),
+      buttonLink: dmg.browser_download_url,
     };
   }
 
@@ -40,48 +40,44 @@ const getPlatformSpecificContent = (assets) => {
     const exe = find(assets, value => value.name.split('.').pop() === 'exe');
     return {
       buttonText: 'Download Installer',
-      onClickHandler: () => openExternalLink(exe.browser_download_url),
+      buttonLink: exe.browser_download_url,
     };
   }
 
   if (isLinux()) {
     return {
       buttonText: 'Open GitHub Release',
-      onClickHandler: () => openExternalLink('https://github.com/complexdatacollective/Server/releases/latest'),
+      buttonLink: 'https://github.com/complexdatacollective/Server/releases/latest',
     };
   }
 
   return {
     buttonText: 'Open Download Page',
-    onClickHandler: () => openExternalLink('https://networkcanvas.com/download.html'),
+    buttonLink: 'https://networkcanvas.com/download.html',
   };
 };
 
-const checkEndpoint = updateEndpoint => new Promise((resolve) => {
-  const currentVersion = remote.app.getVersion();
-
+export const checkEndpoint = (updateEndpoint, currentVersion) =>
   fetch(updateEndpoint)
     .then(response => response.json())
     .then(({ name, body, assets }) => {
       if (compareVersions.compare(currentVersion, name, '<')) {
-        resolve({
+        return {
           newVersion: name,
           releaseNotes: body,
-          releaseButtonContent: getPlatformSpecificContent(assets),
-        });
+          releaseAssets: assets,
+        };
       }
-
       // eslint-disable-next-line no-console
       console.info(`No update available (current: ${currentVersion}, latest: ${name}).`);
-      resolve(false);
+      return false;
     })
     .catch((error) => {
       // eslint-disable-next-line no-console
       console.warn('Error checking for updates:', error);
-      resolve(false); // Don't reject, as we don't want to handle this error - just fail silently.
+      // Don't reject, as we don't want to handle this error - just fail silently.
+      return Promise.resolve(false);
     });
-});
-
 
 const useUpdater = (updateEndpoint, timeout = 0) => {
   const dispatch = useDispatch();
@@ -93,18 +89,18 @@ const useUpdater = (updateEndpoint, timeout = 0) => {
   };
 
   const showReleaseNotes = (releaseNotes, releaseButtonContent) => {
-    const { buttonText, onClickHandler } = releaseButtonContent;
+    const { buttonText, buttonLink } = releaseButtonContent;
 
     dispatch(dialogActions.openDialog({
       type: 'Confirm',
       title: 'Release Notes',
       confirmLabel: buttonText,
-      onConfirm: onClickHandler,
+      onConfirm: () => openExternalLink(buttonLink),
       message: (
         <div className="dialog-release-notes allow-text-selection">
           <p>
             Please read the following release notes carefully, as changes in the software
-            may impact your ability to recieve and export data.
+            may impact your ability to receive and export data.
           </p>
           <ReactMarkdown
             className="dialog-release-notes__notes"
