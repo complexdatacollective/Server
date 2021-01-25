7262123f94bf6b7e7ea15530ce9de9dd80ea2953
Steve Mckellar <steve@wwqrd.uk>
2021-01-25 13:05:15 +0000

    Work in progress, add resolver endpoints to AdminService

--- src/main/data-managers/ResolverManager.js
+++ src/main/data-managers/ResolverManager.js
@@ -4,7 +4,7 @@ const { last, get } = require('lodash');
 const { RequestError, ErrorMessages } = require('../errors/RequestError');
 const { getNetworkResolver } = require('../utils/getNetworkResolver');
 const { transformSessions } = require('../utils/resolver/transformSessions');
-const { formatSessionAsNetwork, insertEgoInNetworks } = require('../utils/formatters/network');
+// const { formatSessionAsNetwork, insertEgoInNetworks } = require('../utils/formatters/network');
 
 const defaultNetworkOptions = {
   enableEntityResolution: true,
@@ -20,7 +20,7 @@ class ResolverManager {
     this.protocolManager = protocolManager;
   }
 
-  getNetwork(
+  getResolvedNetwork(
     protocol,
     networkOptions,
   ) {
@@ -52,9 +52,9 @@ class ResolverManager {
   }
 
   getSessionNetworks(protocolId) {
-    return this.protocolManager.sessionDb.findAll(protocolId, null, null)
-      .then(sessions => sessions.map(formatSessionAsNetwork))
-      .then(networks => insertEgoInNetworks(networks));
+    return this.protocolManager.sessionDb.findAll(protocolId, null, null);
+      // .then(sessions => sessions.map(formatSessionAsNetwork))
+      // .then(networks => insertEgoInNetworks(networks));
   }
 
   resolveProtocol(
@@ -76,6 +76,77 @@ class ResolverManager {
 
     return this.getNetwork(protocol, options)
       .then(([network]) => getNetworkResolver(requestId, command, protocol.codebook, network));
+  }
+
+
+  // resolutions is in format:
+  // [{ id, date }, ...]
+  getResolutionSessionCounts(protocolId, resolutions = []) {
+    return this.sessionDb.findAll(protocolId, null, { updatedAt: 1 })
+      .then((sessions) => {
+        if (resolutions.length === 0) { return { _unresolved: sessions.length }; }
+        const counts = sessions
+          .reduce((acc, session) => {
+            const { _id } = findLast(resolutions, ({ _date }) => _date > session.updatedAt) || { id: '_unresolved' };
+            return {
+              ...acc,
+              [_id]: get(acc, _id, 0) + 1,
+            };
+          }, {});
+
+        return counts;
+      });
+  }
+
+  getResolutions(protocolId) {
+    return this.resolverDB.getResolutions(protocolId)
+      .then(resolutions => resolutions.map(formatResolution));
+  }
+
+  getResolutionsIndex(protocolId) {
+    return this.getResolutions(protocolId)
+      .then(resolutions =>
+        this.getResolutionSessionCounts(protocolId, resolutions)
+          .then((sessionCounts) => {
+            const unresolved = get(sessionCounts, '_unresolved', 0);
+
+            const resolutionsWithCount = resolutions
+              .map(resolution => ({
+                ...resolution,
+                _sessionCount: get(sessionCounts, resolution._id, 0),
+              }));
+
+            return { resolutions: resolutionsWithCount, unresolved };
+          }),
+      );
+  }
+
+  saveResolution(protocolId, parameters, transforms) {
+    return this.resolverDB.insertResolution(protocolId, parameters, transforms);
+  }
+
+  // Delete all resolutions after and INCLUDING resolutionId
+  deleteResolutionsSince(protocolId, resolutionId) {
+    return this.getResolutions(protocolId)
+      // Get all resolutions up to and including resolutionId
+      .then((resolutions) => {
+        const resolutionIndex = resolutions
+          .findIndex(resolution => resolution._id === resolutionId);
+
+        return resolutions.slice(resolutionIndex);
+      })
+      .then(resolutions => resolutions.map(({ _id }) => _id))
+      .then(resolutionIds =>
+        this.resolverDB.deleteResolutions(resolutionIds)
+          .then(() => resolutionIds),
+      );
+  }
+
+  // Delete all resolutions related to a protocol
+  // Used when we delete that protocol, or sessions connected
+  // to that protocol
+  deleteProtocolResolutions(protocolId) {
+    return this.resolverDB.deleteProtocolResolutions(protocolId);
   }
 }
 
---
+++ src/main/utils/formatters/network.js
@@ -0,0 +1,168 @@
+const { includes } = require('lodash');
+const bigInt = require('big-integer');
+
+const getQuery = require('../network-query/query').default;
+const getFilter = require('../network-query/filter').default;
+
+// TODO: share with other places this is defined
+const nodePrimaryKeyProperty = '_uid';
+const egoProperty = '_egoID';
+const caseProperty = 'caseId';
+const entityTypeProperty = '_type'; // NC sends as 'type' at the top level, but this will allow us to also look for a user attribute named type
+
+const nodeAttributesProperty = 'attributes';
+
+const getEntityAttributes = node => (node && node[nodeAttributesProperty]) || {};
+
+const convertUuidToDecimal = uuid => (
+  // BigInt support is in node 10.4, this poly-fills for now
+  uuid ? bigInt(uuid.toString().replace(/-/g, ''), 16).toString(10) : uuid
+);
+
+// TODO: _meta: { id, caseID }
+const unionOfNetworks = networks =>
+  networks.reduce((union, network) => ({
+    nodes: [...union.nodes, ...network.nodes],
+    edges: [...union.edges, ...network.edges],
+    // ego is an object for a single network, and an array for a previously unified network
+    ego: [...union.ego, ...(!Array.isArray(network.ego) ? [network.ego] : network.ego)],
+  }), { nodes: [], edges: [], ego: [], _id: '' });
+
+const processEntityVariables = (entity, variables) => ({
+  ...entity,
+  attributes: Object.keys(getEntityAttributes(entity)).reduce(
+    (accumulatedAttributes, attributeName) => {
+      const attributeData = getEntityAttributes(entity)[attributeName];
+      if (variables[attributeName] && variables[attributeName].type === 'categorical') {
+        const optionNames = variables[attributeName].options || [];
+        const optionData = optionNames.reduce((accumulatedOptions, optionName) => (
+          {
+            ...accumulatedOptions,
+            [`${attributeName}_${optionName.value}`]: !!attributeData && includes(attributeData, optionName.value),
+          }
+        ), {});
+        return { ...accumulatedAttributes, ...optionData };
+      } else if (variables[attributeName] && variables[attributeName].type === 'layout') {
+        const layoutAttrs = {
+          [`${attributeName}_x`]: attributeData && attributeData.x,
+          [`${attributeName}_y`]: attributeData && attributeData.y,
+        };
+        return { ...accumulatedAttributes, ...layoutAttrs };
+      }
+      return { ...accumulatedAttributes, [attributeName]: attributeData };
+    }, {}),
+});
+
+/**
+ * Run the query on each network; filter for those which meet the criteria (i.e., where the query
+ * evaluates to `true`).
+ * @param  {Object[]} networks An array of NC networks
+ * @param  {Object} inclusionQueryConfig a query definition with asserting rules
+ * @return {Object[]} a subset of the networks
+ */
+const filterNetworksWithQuery = (networks, inclusionQueryConfig) =>
+  (inclusionQueryConfig ? networks.filter(getQuery(inclusionQueryConfig)) : networks);
+
+/**
+ * Filter each network based on the filter config.
+ * @param  {Object[]} networks An array of NC networks
+ * @param  {Object} filterConfig a filter definition with rules
+ * @return {Object[]} a copy of `networks`, each possibly containing a subset of the original
+ */
+const filterNetworkEntities = (networks, filterConfig) => {
+  if (!filterConfig || !filterConfig.rules || !filterConfig.rules.length) {
+    return networks;
+  }
+  const filter = getFilter(filterConfig);
+  return networks.map(network => filter(network));
+};
+
+// Strictly caseId is not the egoId, but we probably need both of these bits of info
+const insertNetworkEgo = network => ({
+  ...network,
+  nodes: network.nodes.map(node => ({
+    [egoProperty]: network.ego[nodePrimaryKeyProperty],
+    [caseProperty]: network.sessionVariables[caseProperty],
+    ...node,
+  })),
+  edges: network.edges.map(edge => ({
+    [egoProperty]: network.ego[nodePrimaryKeyProperty],
+    [caseProperty]: network.sessionVariables[caseProperty],
+    ...edge,
+  })),
+  ego: {
+    ...network.sessionVariables,
+    ...network.ego,
+  },
+});
+
+const insertEgoInNetworks = networks => (
+  networks.map(network => insertNetworkEgo(network))
+);
+
+const transposedCodebookVariables = (sectionCodebook, definition) => {
+  if (!definition.variables) { // not required for edges
+    sectionCodebook[definition.name] = definition; // eslint-disable-line no-param-reassign
+    return sectionCodebook;
+  }
+
+  const displayVariable = definition.variables[definition.displayVariable];
+
+  const variables = Object.values(definition.variables).reduce((acc, variable) => {
+    acc[variable.name] = variable;
+    return acc;
+  }, {});
+
+  sectionCodebook[definition.name] = { // eslint-disable-line no-param-reassign
+    ...definition,
+    displayVariable: displayVariable && displayVariable.name,
+    variables,
+  };
+
+  return sectionCodebook;
+};
+
+const transposedCodebookSection = (section = {}) =>
+  Object.values(section).reduce((sectionCodebook, definition) => (
+    transposedCodebookVariables(sectionCodebook, definition)
+  ), {});
+
+const transposedCodebook = (codebook = {}) => ({
+  edge: transposedCodebookSection(codebook.edge),
+  node: transposedCodebookSection(codebook.node),
+  ego: transposedCodebookVariables({}, { ...codebook.ego, name: 'ego' }).ego,
+});
+
+const formatSessionAsNetwork = (session) => {
+  // eslint-disable-next-line no-underscore-dangle
+  const id = session && session._id;
+  const caseID = session && session.data &&
+    session.data.sessionVariables &&
+    // eslint-disable-next-line no-underscore-dangle
+    session.data.sessionVariables.caseId;
+
+  return ({
+    ...session.data,
+    caseId: caseID,
+    _id: id,
+    _date: session.createdAt,
+  });
+};
+
+module.exports = {
+  convertUuidToDecimal,
+  filterNetworkEntities,
+  filterNetworksWithQuery,
+  getEntityAttributes,
+  insertEgoInNetworks,
+  nodeAttributesProperty,
+  entityTypeProperty,
+  egoProperty,
+  caseProperty,
+  nodePrimaryKeyProperty,
+  processEntityVariables,
+  transposedCodebook,
+  transposedCodebookSection,
+  unionOfNetworks,
+  formatSessionAsNetwork,
+};
