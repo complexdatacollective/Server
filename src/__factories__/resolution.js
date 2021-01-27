6676209938ea0f9a16efae086cff9208213c145a
Steve Mckellar <steve@wwqrd.uk>
2021-01-27 12:59:10 +0000

    Add modules nedded for transformSessions tests

---
+++ src/__factories__/resolution.js
@@ -0,0 +1,24 @@
+const { Factory } = require('rosie');
+const { DateTime } = require('luxon');
+const uuid = require('uuid');
+const { transform } = require('./transform');
+
+const resolution = new Factory()
+  .option('network', null)
+  .option('transformCount', null)
+  .option('attributes', {})
+  .attr('_id', () => uuid())
+  .sequence('_date', i => DateTime.local().minus({ days: i }).toJSDate())
+  .attr(
+    'transforms',
+    ['transformCount', 'network', 'attributes'],
+    (transformCount, network, attributes) => {
+      const size = transformCount || Math.ceil(network.nodes.length * 0.3);
+      return transform.buildList(size, { attributes }, { network });
+    },
+  );
+
+module.exports = {
+  default: resolution,
+  resolution,
+};
