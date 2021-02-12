70a42c0349b6ee203464ff771ed029acf9e0b875
Steve Mckellar <steve@wwqrd.uk>
2021-02-12 13:45:43 +0000

    First pass at castOrphanEgos

--- src/main/utils/resolver/castOrphanEgosAsEgoNodes.js
+++ src/main/utils/resolver/castOrphanEgosAsEgoNodes.js
@@ -1,8 +1,43 @@
 const castOrphanEgosAsEgoNode = (sessions, protocol, resolvedNetwork) => {
   // Collect all egos from sessions
+  const egos = sessions.reduce(
+    (acc, { ego }) => ({
+      ...acc,
+      [ego._uid]: { ...ego, type: '_ego' }, // eslint-disable-line no-underscore-dangle
+    }),
+    {},
+  );
+  const egoIds = Object.keys(egos);
+
   // Create ego type in codebook
-  // insert egos into network as ego typ
-  return [resolvedNetwork, protocol];
+  const modifiedProtocol = {
+    ...protocol,
+    codebook: {
+      ...protocol.codebook,
+      ego: {},
+      node: {
+        ...protocol.codebook.node,
+        _ego: protocol.codebook.ego,
+      },
+    },
+  };
+
+  const modifiedNetwork = {
+    ...resolvedNetwork,
+    nodes: resolvedNetwork.map(
+      (node) => {
+        const id = node._uid;
+        if (egoIds.includes(id)) {
+          return ego[id];
+        }
+
+        return node;
+      },
+    );
+  };
+
+  // insert egos into network as ego type
+  return [modifiedNetwork,  modifiedProtocol];
 };
 
 module.exports = castOrphanEgosAsEgoNode;
