567f072f628f32f15ce1b965e17279467bf36d4d
Steve Mckellar <steve@wwqrd.uk>
2021-02-18 14:24:55 +0000

    Update review to include more explanation

--- src/renderer/containers/EntityResolver/Review.js
+++ src/renderer/containers/EntityResolver/Review.js
@@ -12,23 +12,39 @@ const Review = ({
 
   return (
     <div className="review">
+      <div className="review__summary">
+        <p>Below is a summary of your resolution actions.</p>
+      </div>
+
       <table className="review__stats">
         <tbody>
           <tr>
-            <th>Total matches</th>
             <td>{matches.length}</td>
+            <th>
+              <h4>Total matches found</h4>
+              <p>Number of pairs returned by your script</p>
+            </th>
           </tr>
           <tr>
-            <th>Resolved</th>
             <td>{resolvedCount}</td>
+            <th>
+              <h4>Resolved pairs</h4>
+              <p>Number of pairs you indicated were valid matches</p>
+            </th>
           </tr>
           <tr>
-            <th>Implicit</th>
-            <td>{implicitCount}</td>
-          </tr>
-          <tr>
-            <th>Skipped</th>
             <td>{skipCount}</td>
+            <th>
+              <h4>Skipped pairs</h4>
+              <p>Number of pairs you indicated were not matches</p>
+            </th>
+          </tr>
+          <tr title="If NODE_A = NODE_B and NODE_C=NODE_B then NODE_A=NODE_C">
+            <td>{implicitCount}</td>
+            <th>
+              <h4>Inferred pairs</h4>
+              <p>Number of pairs that were resolved automatically by transitive relation.</p>
+            </th>
           </tr>
         </tbody>
       </table>
